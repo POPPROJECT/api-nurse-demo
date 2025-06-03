e0432fd4ab0aba95fb7bf03958194a311fd70046
TRIPOP <tripop.raksa@gmail.com>
2025-06-04 05:44:06 +0700

    back-20 commit

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,18 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
+    // เชื่อมต่อ database ตอนที่ module เริ่มทำงาน
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    // ปิดการเชื่อมต่อ ตอนที่แอปปิดตัวลง
+    await this.$disconnect();
   }
 }
