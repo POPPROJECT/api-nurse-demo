25eb1b546a945fe909618053091b16ddbf737526
TRIPOP <tripop.raksa@gmail.com>
2025-06-04 05:10:59 +0700

    back-18 commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get()
+  healthCheck(): string {
+    // เมื่อ Railway เรียกมาที่ URL หลัก มันจะได้รับ 200 OK พร้อมข้อความนี้
+    return 'API is healthy and running!';
+  }
 }
